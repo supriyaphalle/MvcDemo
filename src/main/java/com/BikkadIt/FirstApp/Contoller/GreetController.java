c324c56c7a963793ccf9f5debf1b01fb8a978623
supriyaphalle <54026790+supriyaphalle@users.noreply.github.com>
2023-08-14 22:16:11 +0530

    Update GreetController.java

--- src/main/java/com/BikkadIt/FirstApp/Contoller/GreetController.java
+++ src/main/java/com/BikkadIt/FirstApp/Contoller/GreetController.java
@@ -4,11 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/greetController")
+
 public class GreetController {
 
 	@GetMapping("/wel")
